c9a06217753a414db527fb643c1891c3c53351f1
Justin Caovan <jtcaovan@gmail.com>
2022-09-14 19:16:36 -0700

    feat(client): initialize markdown converter library

--- src/components/MainBodyContainer.tsx
+++ src/components/MainBodyContainer.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
 export default function MainBodyContainer() {
-	return <></>;
+	const [markdown, setMarkdown] = useState("# dello");
+
+	return (
+		<body className="h-100 w-100">
+			<aside></aside>
+
+			<ReactMarkdown># Hello</ReactMarkdown>
+		</body>
+	);
 }
